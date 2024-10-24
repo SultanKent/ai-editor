770b9522acd72f54aa62ac01933018e2393467e2
Raul Kuanyshev <ffkirito6@gmail.com>
2024-10-24 19:06:37 +0500

    hashroute

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import GrammarCorrection from './components/services/GrammarCorrection';
 import { Service } from './components/Service';
 import Auth from './components/Auth';
